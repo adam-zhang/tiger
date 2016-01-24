2376a8e6a0870b1b8cb4eddf55876802c7916385
Adam-Zhang <mingpingzhang@163.com>
2016-01-24 23:00:47 +0800

    first commit

---
+++ src/gui/gui.cpp
@@ -0,0 +1,9 @@
+#include "gui.h"
+#include <iostream>
+
+int guiMain(int argc, char** argv)
+{
+	std::cout << "Hello world" << std::endl;
+	return 0;
+}
+
---
+++ src/gui/gui.h
@@ -0,0 +1,6 @@
+#ifndef __GUI__H
+#define __GUI__H
+
+int guiMain(int argc, char** argv);
+#endif//__GUI__H
+
