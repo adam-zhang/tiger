5cc9405855f24de7be318504418b4c40dfab97f1
Adam-Zhang <mingpingzhang@163.com>
2016-01-25 23:03:45 +0800

    Hello

--- src/gui/AboutDialog.cpp
+++ src/gui/AboutDialog.cpp
@@ -1,10 +1,55 @@
 #include "AboutDialog.h"
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGridLayout>
+#include <QLabel>
+#include <QDialogButtonBox>
 
 AboutDialog::AboutDialog(QWidget* parent)
 	: QDialog(parent)
 {
+	initialize();
 }
 
 AboutDialog::~AboutDialog()
 {
+}
+
+void AboutDialog::initialize()
+{
+	QVBoxLayout * layout = new QVBoxLayout(this);
+	layout->addLayout(mainPart());
+	layout->addWidget(buttonPart());
+}
+
+QLayout* AboutDialog::mainPart()
+{
+	QHBoxLayout* layout = new QHBoxLayout;
+	layout->addWidget(pictureWidget());
+	layout->addLayout(info());
+	return layout;
+}
+
+QWidget* AboutDialog::pictureWidget()
+{
+	QWidget* widget = new QWidget;
+	return widget;
+}
+
+QLayout* AboutDialog::info()
+{
+	QGridLayout* layout = new QGridLayout;
+	layout->addWidget(new QLabel(tr("Atuhor:")), 0, 0);
+	layout->addWidget(new QLabel(tr("Mingping (Adam) Zhang")), 0, 1);
+	layout->addWidget(new QLabel(tr("E-mail:")), 1, 0);
+	layout->addWidget(new QLabel(tr("mingpingzhang@163.com")), 1, 1);
+	return layout;
+}
+
+QWidget* AboutDialog::buttonPart()
+{
+	QDialogButtonBox* box = new QDialogButtonBox;
+	box->addButton(QDialogButtonBox::Ok);
+	connect(box, SIGNAL(accepted()), this, SLOT(close()));
+	return box;
 }
--- src/gui/AboutDialog.h
+++ src/gui/AboutDialog.h
@@ -3,11 +3,19 @@
 
 #include <QDialog>
 
+class QLayout;
+
 class AboutDialog : public QDialog
 {
 	Q_OBJECT
 public:
 	AboutDialog(QWidget* parent = 0);
 	virtual ~AboutDialog();
+private:
+	void initialize();
+	QLayout* mainPart();
+	QWidget* buttonPart();
+	QLayout* info();
+	QWidget* pictureWidget();
 };
 #endif//__ABOUTDIALOG__H
